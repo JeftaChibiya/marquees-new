aac798ca37d138e698143e8c0a680a0f4c03fe10
Jefta Chibiya <jeftachibiya@hotmail.co.uk>
2018-02-10 10:11:54 +0000

    Revised Prototype-2

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
