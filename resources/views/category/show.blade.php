ac95d62061e70b7a0cf69a318bab0b061e5c9da2
Jefta Chibiya <jeftachibiya@hotmail.co.uk>
2018-02-23 15:18:18 +0000

    Event Cover 1

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -51,10 +51,9 @@
           <div class="column is-8">
             <div class="bg-cover" style="background-image: url({{ $categoryEventCover }})">
               <div class="overlay center-all">                  
-                <p class="subtitle is-5 white">{{ $category->name }}</p>                              
+                <p class="subtitle is-3 white">{{ $category->name }}</p>                              
               </div>           
-            </div>           
-            <p class="subtitle is-4 has-text-centered"> {{ $category->name }} </p>          
+            </div>                    
           </div>
 
           <div class="column">  
