7c1027d96abce0c787190598ab5f1c7fd3143aa4
Jefta Chibiya <jeftachibiya@hotmail.co.uk>
2018-03-06 15:00:45 +0000

    About us styling

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,52 +4,53 @@
 
 @section('content')
 
-<div class="category-intro bg-center" style="background-image: url('/images/features/party/cat_party.jpg')">     
-    <div class="container">
-        <div class="has-text-centered">
-            <p class="title is-1 white"> <b>About Us</b></p>         
-        </div>      
-    </div>  
+<div class="container">
+    <section class="section">
+        <p class="title is-3"> <b>About Us</b></p> 
+        <hr/>           
+    </section>                        
 </div>    
-
-<br/>
 
 <div class="container">
     <section class="section">
-        <div class="columns">
+    <div class="columns">
 
-            <div class="column is-6">
-                <p class="title is-2"><b>We help you build the perfect marquee</b></p>
-                 <p>
-                    We design and manufacture marquees to suit your 
-                    requirements, whether for social or industrial purposes. 
-                    High durability and safety is prioritised on all structures.                       
-                 </p><br/>
-                 <a href="/show-categories" class="is-link">Browse examples and real use cases</a>
-            </div>
-
-            <div class="column is-4">
-                <img src="/images/features/party/party.png" class="img-circle" style="100%" alt="">
-            </div>
+        <div class="column is-6">
+            <p class="title is-2"><b>We help you build the perfect marquee</b></p>
+                <p>
+                We design and manufacture marquees to suit your 
+                requirements, whether for social or industrial purposes. 
+                High durability and safety is prioritised on all structures.                       
+                </p><br/>
+                <a href="/show-categories" class="is-link">Browse examples and real use cases</a>
         </div>
 
-        <br/><br/>
+        <div class="column is-4">
+            <img src="/images/features/party/party.png" class="img-circle" style="100%" alt="">
+        </div>
+    </div>
 
-        <div class="columns">
-            <div class="column is-6">
-                <img src="/images/features/featured_imgs/blueprint.jpg" style="width: 100%" alt="">
-            </div>
-            <div class="column">
-                <p class="title is-2"><b>Delivery is tailored to your needs</b></p>
-                 <p>
-                    We offer options to build your marquee on site or deliver pre-built structures. 
-                    Additionally, your marquee can be delivered at a time convinient to you.                    
-                 </p>
-            </div>            
-        </div>        
-    </section> <br/>
+    <hr class="hr-divider">
+    <br/>
 
+    <div class="columns">
+        <div class="column is-6">
+            <img src="/images/features/featured_imgs/blueprint.jpg" style="width: 100%" alt="">
+        </div>
+        <div class="column">
+            <p class="title is-2"><b>Delivery is tailored to your needs</b></p>
+                <p>
+                We offer options to build your marquee on site or deliver pre-built structures. 
+                Additionally, your marquee can be delivered at a time convinient to you.                    
+                </p>
+        </div>            
+    </div>        
+    </section> 
+</div>    
 
+<hr class="hr-divider">    
+
+<div class="container">
     <div class="content-container">
         <p class="title is-3 has-text-centered">Trusted and Accreditted products</p>         
         <section class="section">           
@@ -60,6 +61,6 @@
                 </div>
             </div> 
         </section>       
-    </div>
+    </div>        
 </div>
 @endsection
